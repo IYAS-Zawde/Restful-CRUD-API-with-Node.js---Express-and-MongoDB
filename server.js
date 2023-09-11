77bc3517c5eebbb7a5c9dd09245c840767c6e2f5
IYAS Zawde <iyas.zawde@gmail.com>
2023-09-11 22:42:05 +0300

    Updating Products
    We add code in this step that allows us to update products in the database using JSON format in the request body or URL encoded format.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Product = require('./models/productModel')
+
 const app = express()
 app.use(express.json());
-
+app.use(express.urlencoded({extended: false}))
 //routes
 app.get('/', (req, res)=>{
     res.send('Hello Node API')
@@ -43,6 +44,20 @@ app.post('/product', async (req, res)=>{
     }
 });
 
+app.put('/products/:id', async (req, res)=>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID: ${id}`})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+});
 
 mongoose.
 connect('mongodb+srv://admin:RBC4jCrtzhRjaXim@db-restfullapinodejs.fcofqdz.mongodb.net/Node-API?retryWrites=true&w=majority')
