a7377cfc878f3cc363ab789b7ca7fc22cf50e294
IYAS Zawde <iyas.zawde@gmail.com>
2023-09-11 15:19:54 +0300

    Setting MangoDB
    Create a MangoDB online and connect to it from our application
    - setup database online
    - create mongoose variable
    - connect to DB using given URL
    - make sure to run the server after you connect to DB by moving "app.listen" instruction to the "then" block

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express()
 
 //routes
 app.get('/', (req, res)=>{
     res.send('Hello Node API')
 })
+app.get('/blog', (req, res)=>{
+    res.send('Hello Blog, This is after adding Nodemon')
+})
 
-app.listen(3000, ()=>{
-    console.log('Node API app is running on port 3000')
+
+
+mongoose.
+connect('mongodb+srv://admin:RBC4jCrtzhRjaXim@db-restfullapinodejs.fcofqdz.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('connected to MangoDB');
+    app.listen(3000, ()=>{
+        console.log('Node API app is running on port 3000')
+    });
+}).catch((error)=>{
+    console.log(error);
 })
