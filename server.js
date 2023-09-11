9cad7f180ff98e270f7785116341a6fea2278e84
IYAS Zawde <iyas.zawde@gmail.com>
2023-09-11 19:58:48 +0300

    Getting Products from DB
    In this stage, we code the functions that allow us to acquire a list of products from the database and a single product by its id.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/productModel')
 const app = express()
+app.use(express.json());
 
 //routes
 app.get('/', (req, res)=>{
@@ -10,6 +12,36 @@ app.get('/blog', (req, res)=>{
     res.send('Hello Blog, This is after adding Nodemon')
 })
 
+app.get('/products', async (req, res)=>{
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.get('/products/:id', async (req, res)=>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.post('/product', async (req, res)=>{
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+});
 
 
 mongoose.
